d41c3a254a81526bb34ead4ea7418bd035f000cf
Ashish Kumar <trojan2277@gmail.com>
2021-12-30 21:15:13 +0530

    new update

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import useScrollPosition from "use-scroll-position";
 import "../css/style.css";
-import "../css/skin/skin-2.css";
+import "../css/skin/skin.css";
 import "../styles/globals.css";
 import "../styles/switcher.css";
 
